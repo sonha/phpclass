f26b0efae6faf1c8799f8a0dffcf0170a3927c3c
Ha Anh Son <hason61vn@gmail.com>
2014-04-26 11:53:30 +0700

    first commit

--- training_sql/connect.php
+++ training_sql/connect.php
@@ -14,19 +14,4 @@ $db=mysql_select_db('sql_training',$conn);
 if(!$db){
 	die("Khong the ket noi voi database");
 }
-function sqlquery($arr, $tbl, $param=''){
-	$query=mysql_query("SELECT $arr FROM $tbl $param");
-	$result="";
-	while($rows=mysql_fetch_array($query)){
-		$result[]=$rows;
-	}
-//var_dump($result);
-	if($result){
-		return $result;
-	}
-	else{
-		return false;
-	}
-	
-}
 ?>
--- training_sql/index.php
+++ training_sql/index.php
@@ -1,31 +1,34 @@
 <?php
-	include('connect.php');
-	$pr_query=sqlquery("*", "product", "WHERE category_id=6 AND status=1");
-	//var_dump($pr_query);
+include('connect.php');
+$query = "SELECT * FROM  product WHERE category_id=6 AND status=1";
+$result = mysql_query($query);
+while ($rows = mysql_fetch_array($result)) {
+    $data[] = $rows;
+}
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <title></title>
 </head>
 <body>
-	<table border="1px">
-	<tr>
-		<td>Name</td>
-		<td>category_id</td>
-		<td>status</td>
-	</tr>
-		<?php foreach ($pr_query as $key => $val):
-			$name=$val["name"];
-			$category_id=$val["category_id"];
-			$status=$val["status"];
-		?>
-		<tr>
-			<td><?php echo $name; ?></td>
-			<td><?php echo $category_id; ?></td>
-			<td><?php echo $status; ?></td>
-		</tr>
-		<?php endforeach; ?>
-	</table>
+<table border="1px">
+    <tr>
+        <td>Name</td>
+        <td>category_id</td>
+        <td>status</td>
+    </tr>
+    <?php foreach ($data as $key => $val):
+        $name = $val["name"];
+        $category_id = $val["category_id"];
+        $status = $val["status"];
+        ?>
+        <tr>
+            <td><?php echo $name; ?></td>
+            <td><?php echo $category_id; ?></td>
+            <td><?php echo $status; ?></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
 </body>
 </html>
